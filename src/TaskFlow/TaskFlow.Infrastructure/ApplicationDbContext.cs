fa4d70aba1a2f90d6a37474972360cff56f97f71
emon <maminemon@gmail.com>
2025-04-23 10:30:10 +0600

    Identity table added

--- src/TaskFlow/TaskFlow.Infrastructure/ApplicationDbContext.cs
+++ src/TaskFlow/TaskFlow.Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TaskFlow.Domain.Entities;
 
 namespace TaskFlow.Web.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
